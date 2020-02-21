5d033db83dcd3206a3f0ad8796093c81e3459ff6
Askar Karasaev <askar.karasaev@gmail.com>
2020-02-22 02:40:51 +0900

    Image display fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -70,7 +70,7 @@ class BookingsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_booking
-    @booking = Booking.find(params[:id])
+    @booking = @customer.bookings.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
